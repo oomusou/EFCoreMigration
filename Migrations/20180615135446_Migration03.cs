d6de2ce6e12b1f7099c614f10055f6218066b06a
oomusou <oomusou@gmail.com>
2018-06-15 21:55:59 +0800

    Add Migration03

---
+++ Migrations/20180615135446_Migration03.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreMigration.Migrations
+{
+    public partial class Migration03 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerID", "Age", "Name" },
+                values: new object[] { 1, 18, "Sam" });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerID", "Age", "Name" },
+                values: new object[] { 2, 19, "Kevin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerID",
+                keyValue: 2);
+        }
+    }
+}
