96680c029e7ef9f74ede9127956b06cc27f2607d
oomusou <oomusou@gmail.com>
2018-06-15 21:29:00 +0800

    Alter Name from text to varchar in Customer entity

--- Customer.cs
+++ Customer.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EFCoreMigration
 {
     public class Customer
     {
         public int CustomerID { get; set; }
+        
+        [StringLength(20)]
         public string Name { get; set; }
         public int Age { get; set; }
     }
