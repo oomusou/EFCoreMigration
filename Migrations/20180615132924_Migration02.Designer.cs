6c561bf6ee98ba254a85dc7ffa6447d92b1eefff
oomusou <oomusou@gmail.com>
2018-06-15 21:29:45 +0800

    Add Migration02

--- Migrations/20180615132924_Migration02.Designer.cs
+++ Migrations/20180615132924_Migration02.Designer.cs
@@ -2,15 +2,17 @@
 using EFCoreMigration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace EFCoreMigration.Migrations
 {
     [DbContext(typeof(EFLabDbContext))]
-    partial class EFLabDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180615132924_Migration02")]
+    partial class Migration02
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20180615132924_Migration02.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreMigration.Migrations
+{
+    public partial class Migration02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
